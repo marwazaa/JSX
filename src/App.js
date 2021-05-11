634d4b8e67cf4a6ab9cb477c22aa458ae27d359e
unknown <marwa.zaabi2012@gmail.com>
2021-05-11 21:13:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import './App.css';
 import imagesource from "./imagesource.jpg";
 import "./style.css";
 
 function App() {
   return (
-    <div>
-    <div style={{border:'solid 1px black', maxWidth: "100vw" }}>
-      <h1 className="title red">marwa</h1>
-    <img className="img" src={imagesource} alt="imageInSrc" />
-    <img className="img" src="imageInPublic.jpg" alt="imageInPublic" />
-    </div>
-    <video className="video1" width='750' hight='500' controls />
+  <div>
 
-    <source src="myVideo.mp4" type="video/mp4" />
+    <div style={{border:'solid 1px black', maxWidth: "100vw" }}>
+
+       <h1 className="title red">marwa zaabi</h1>
+
+       <div className="imgimg">
+           <img className="img" src={imagesource} alt="imageInSrc"/>
+           <img className="img" src="imageInPublic.jpg" alt="imageInPublic"/>
+        </div>
+
+    </div>
+
+    <video style={{width:"320", height:"240" }} controls>
+      <source src="myVideo.mp4" type="video/mp4"></source>
+    </video>
+    
   </div>
+
   );
 }
 
